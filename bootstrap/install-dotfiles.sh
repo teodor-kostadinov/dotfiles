45a9624dc9429056729ab94d8d76d8798daa6244
Teodor Kostadinov <teodor@privatemail.us>
2021-06-12 03:54:42 +0300

    Adding an install script

---
+++ bootstrap/install-dotfiles.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cd; curl -#L https://github.com/teodor-kostadinov/dotfiles/tarball/main | tar -xzv --strip-components 1 --exclude={README.md,LICENSE,bootstrap,.gitignore}; source .zshrc
